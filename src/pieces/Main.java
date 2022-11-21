2721191a340bdb35635972ecf6586ba05271cc2c
SPirate9 <118197355+SPirate9@users.noreply.github.com>
2022-11-21 14:07:46 +0100

    Update Main.java

--- src/pieces/Main.java
+++ src/pieces/Main.java
@@ -6,20 +6,20 @@ public class Main {
   static Scanner user_input = new Scanner(System.in);
 
   public static void main(String[] args) {
-	// J'instancie un nouvel échiquier que j'appelle mon échiquier
+
     Echiquier monEchiquier = new Echiquier();
 
     monEchiquier.afficherTableau();
-    // Je demande au plateau de m'afficher le plateau
+
     monEchiquier.mouvement();
-    // Je fais appel a mon scanner
+   
 
     while (monEchiquier.getjeuxEnCours()) {
-      // je fais une boucle 
+  
       monEchiquier.afficherTableau();
-      //tant que le jeux est en cours de m'afficher le tableau
+  
       monEchiquier.mouvement();
-      // ainsi que le scann
+    
 
     }
 
