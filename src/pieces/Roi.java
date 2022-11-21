8518890cbd2ff3c32606af81c54866ff27a5f69c
SPirate9 <118197355+SPirate9@users.noreply.github.com>
2022-11-21 14:09:29 +0100

    Update Roi.java

--- src/pieces/Roi.java
+++ src/pieces/Roi.java
@@ -6,7 +6,7 @@ public class Roi extends Pieces {
 
   @Override
   public String toString() {
-    // La fonction renvoie une représentation sous forme de String de la valeur R
+  
     return String.valueOf(nom);
   }
 
@@ -15,7 +15,5 @@ public class Roi extends Pieces {
     return Math.abs(arriveL - departL) <= 1 && Math.abs(arriveC - departC) <= 1;
   }
 
-  // methode avec override non utilisé a completer
-  // uptadate methode fait !
-  // Roi OK
+ 
 }
